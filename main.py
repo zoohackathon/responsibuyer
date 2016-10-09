005d7e9f7c63593e08b4e584883a762ca199b1ce
Ryan Handy <ryanhandy@Ryans-MacBook-Pro.local>
2016-10-08 19:35:10 -0700

    product details collected

--- main.py
+++ main.py
@@ -16,6 +16,35 @@ def search():
     location = geolocator.reverse(point, language='en')
     country = location.raw['address']['country']
     country_code = location.raw['address']['country_code']
+    cur = conn.cursor()
+    cur.execute("""
+        select p.name, 
+               p.description, 
+               w.genus, 
+               w.species, 
+               w.common_name, 
+               w.conservation_status, 
+               count(*) AS total_trades 
+        from wildlife_trade wt 
+        inner join wildlife w on wt.wildlife_id = w.id 
+        inner join product_category p on wt.product_category_id = p.id 
+        where wt.country_code = 'LA' 
+        GROUP BY p.name, p.description, w.genus, w.species, w.common_name, w.conservation_status
+        ORDER BY total_trades DESC;
+    """)
+    products = []
+    rows = cur.fetchall()
+    for row in rows:
+        product = {}
+        product['name'] = row[0]
+        product['desc'] = row[1]
+        product['animal_genus'] = row[2]
+        product['animal_species'] = row[3]
+        product['animal_name'] = row[4]
+        product['animal_conservation_status'] = row[5]
+        product['ranking'] = row[6]
+        products.append(product)
+    resp['products'] = products
     resp['country'] = country
     resp['country_code'] = country_code
     return json.jsonify(resp)
