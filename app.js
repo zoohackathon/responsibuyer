d08d99f743450cf7c725262a9fe037b405ea8fb1
bonecrushereb <bonecrushereb@gmail.com>
2016-10-08 17:36:26 -0700

    added accordion funcationality to gh-pages app.js

--- app.js
+++ app.js
@@ -1,23 +1,25 @@
+var RSBer = (function(RSBer, $){
+  var _placeSearch;
+  var autocomplete;
+  var isInit = false;
 
-//autocomplete
-var placeSearch, autocomplete;
-
-     function initAutocomplete() {
-
-       autocomplete = new google.maps.places.Autocomplete(
+  var _init = function(){
+    if(!isInit && !!window.google){
+      autocomplete = new window.google.maps.places.Autocomplete(
            /** @type {!HTMLInputElement} */(document.getElementById('autocomplete')),
            {types: ['geocode']});
+      isInit = true;
+    }
+  };
 
-     }
-
-     function geolocate() {
-       if (navigator.geolocation) {
-         navigator.geolocation.getCurrentPosition(function(position) {
+  var _geolocate = function(){
+    if (!!navigator && typeof navigator.geolocation !== 'undefined') {
+         navigator.geolocation.getCurrentPosition(function getPosition(position) {
            var geolocation = {
              lat: position.coords.latitude,
              lng: position.coords.longitude
            };
-           var circle = new google.maps.Circle({
+           var circle = new window.google.maps.Circle({
              center: geolocation,
              radius: position.coords.accuracy
            });
@@ -25,7 +27,6 @@ var placeSearch, autocomplete;
          });
        }
      }
-
 
      //newsfeed
      $(function() {
@@ -59,4 +60,49 @@ var placeSearch, autocomplete;
        $( "#accordion" ).accordion({
          collapsible: true
        });
+  };
+
+  var getNewsfeed = function(){
+     var params = {
+         // Request parameters
+         "q": "illegal animal trade",
+         "count": "10",
+         "offset": "0",
+         "mkt": "en-us",
+         "safeSearch": "Moderate",
+     };
+
+     $.ajax({
+         url: "https://api.cognitive.microsoft.com/bing/v5.0/news/search?" + $.param(params),
+         beforeSend: function(xhrObj){
+             // Request headers
+             xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key","f07fec7721354863a1a6cf6d2a2d05ee");
+         },
+         type: "GET",
+         // Request body
+         data: "{body}",
+     })
+     .done(function(data) {
+         console.log("success");
+         console.log(data);
+     })
+     .fail(function() {
+         console.log("error");
      });
+  };
+
+  return {
+    init: _init,
+    geolocate: _geolocate,
+    getNewsfeed: getNewsfeed
+  };
+
+}(RSBer || {}, jQuery, window.google));
+
+$(document).on('ready', function(){
+  RSBer.init();
+});
+
+function initRSBer() {
+  RSBer.init();
+};
