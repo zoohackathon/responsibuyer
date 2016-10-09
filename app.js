f4e5fae0ae89f586c2cfc5e7ed16d9967137a8e1
Munson <munsoj@amazon.com>
2016-10-08 17:39:33 -0700

    merge with gh-pages

--- app.js
+++ app.js
@@ -26,41 +26,7 @@ var RSBer = (function(RSBer, $){
            autocomplete.setBounds(circle.getBounds());
          });
        }
-     }
-
-     //newsfeed
-     $(function() {
-         var params = {
-             // Request parameters
-             "q": "illegal animal trade",
-             "count": "10",
-             "offset": "0",
-             "mkt": "en-us",
-             "safeSearch": "Moderate",
-         };
-
-         $.ajax({
-             url: "https://api.cognitive.microsoft.com/bing/v5.0/news/search?" + $.param(params),
-             beforeSend: function(xhrObj){
-                 // Request headers
-                 xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key","f07fec7721354863a1a6cf6d2a2d05ee");
-             },
-             type: "GET",
-             // Request body
-             data: "{body}",
-         })
-         .done(function(data) {
-             console.log("success");
-             console.log(data);
-         })
-         .fail(function() {
-             console.log("error");
-         });
-
-       $( "#accordion" ).accordion({
-         collapsible: true
-       });
-  };
+     };
 
   var getNewsfeed = function(){
      var params = {
@@ -89,13 +55,17 @@ var RSBer = (function(RSBer, $){
      .fail(function() {
          console.log("error");
      });
+
+     $( "#accordion" ).accordion({
+         collapsible: true
+       });
   };
 
   return {
     init: _init,
     geolocate: _geolocate,
     getNewsfeed: getNewsfeed
-  };
+  }; 
 
 }(RSBer || {}, jQuery, window.google));
 
